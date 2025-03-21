b833acc87be6e0ff4dd8064ff83f07109b0d0f55
scrs_zdenek <zdenek@scrs.shimano.com.sg>
2025-03-21 12:08:56 +0100

    dev

--- src/Drago/assets/naja.components.js
+++ src/Drago/assets/naja.components.js
@@ -3,15 +3,17 @@ import { Modal, Offcanvas } from "bootstrap";
 
 // Function to initialize Bootstrap components (Modal and Offcanvas)
 function initBsComponents(el) {
-	// Initialize Modals
-	el.querySelectorAll('.modal').forEach((modal) => {
+	// Modal
+	el.querySelectorAll('.modal').forEach(modal => {
 		if (!modal._bsModal) {
-			modal._bsModal = new Modal(modal, { keyboard: false });
+			modal._bsModal = new Modal(modal, {
+				keyboard: false
+			});
 		}
 	});
 
-	// Initialize Offcanvas components
-	el.querySelectorAll('.offcanvas').forEach((offCanvas) => {
+	// Offcanvas
+	el.querySelectorAll('.offcanvas').forEach(offCanvas => {
 		if (!offCanvas._bsOffcanvas) {
 			offCanvas._bsOffcanvas = new Offcanvas(offCanvas);
 		}
@@ -19,31 +21,38 @@ function initBsComponents(el) {
 }
 
 document.addEventListener('DOMContentLoaded', () => initBsComponents(document));
-
 naja.snippetHandler.addEventListener('afterUpdate', (e) => {
 	initBsComponents(e.detail.snippet);
 });
 
 naja.addEventListener('complete', (e) => {
-	const { payload } = e.detail;
+	const payload = e.detail.payload;
+	const doc = document;
 
 	if (payload) {
-		const doc = document;
-
-		// Show modal if specified in payload
 		if (payload.modal) {
-			let modal = doc.querySelector(`#${payload.modal}`) || doc.querySelector(`#${payload.modal} .modal`);
-			modal?._bsModal?.show();
+			let modal = doc.querySelector('#' + payload.modal);
+			if (!modal.classList.contains('modal')) {
+				modal = modal.querySelector('.modal');
+			}
+
+			if (modal?._bsModal) {
+				modal._bsModal.show();
+			}
 		}
 
-		// Show offcanvas if specified in payload
 		if (payload.offcanvas) {
-			let offCanvas = doc.querySelector(`#${payload.offcanvas}`) || doc.querySelector(`#${payload.offcanvas} .offcanvas`);
-			offCanvas?._bsOffcanvas?.show();
+			let offCanvas = doc.querySelector('#' +  payload.offcanvas);
+			if (!offCanvas.classList.contains('offcanvas')) {
+				offCanvas = offCanvas.querySelector('.offcanvas');
+			}
+
+			if (offCanvas?._bsOffcanvas) {
+				offCanvas._bsOffcanvas.show();
+			}
 		}
 
-		// Close any open modal or offcanvas
-		if (payload.close) {
+		if (payload.close){
 			doc.querySelector('.offcanvas.show')?._bsOffcanvas?.hide();
 			doc.querySelector('.modal.show')?._bsModal?.hide();
 		}
